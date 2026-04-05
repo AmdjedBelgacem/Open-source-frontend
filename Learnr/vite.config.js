217e29c379f9e8904bdaca17ec04813b7c0788e5
Amdjed Belgacem <45467302+AmdjedBelgacem@users.noreply.github.com>
2026-04-05 23:25:41 +0100

    replaced api proxy to prod

--- Learnr/vite.config.js
+++ Learnr/vite.config.js
@@ -42,7 +42,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://learnr-backend.vercel.app/',
         changeOrigin: true,
       },
     },
